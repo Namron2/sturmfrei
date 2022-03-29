caebc375f651fb33e624d82eff357ebddb1cef54
Azanen <philippe.beauchemin.1995@gmail.com>
2022-03-29 14:31:02 -0400

    Wall bloque player fix

--- Sturmfrei_Project/Assets/Ingredients/Mur Brisable/MurBrisable.cs
+++ Sturmfrei_Project/Assets/Ingredients/Mur Brisable/MurBrisable.cs
@@ -8,15 +8,20 @@ public class MurBrisable : MonoBehaviour
     public GameObject MurBrise;
     public GameObject MurSolide;
     public AudioSource bedingbedang;
-    public PlayerCollisionSphere playMov;
+    public PlayerCollisionSphere playMov; 
+    List<GameObject> childObjects = new List<GameObject>();
 
-    private void Start()
+
+private void Start()
     {
         bedingbedang = this.gameObject.GetComponent<AudioSource>();
         MurSolide = this.gameObject.transform.GetChild(0).gameObject;
         MurBrise = this.gameObject.transform.GetChild(1).gameObject;
         MurBrise.SetActive(false);
-
+        foreach (Transform child in MurBrise.transform)
+        {
+            childObjects.Add(child.gameObject);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -37,7 +42,13 @@ public class MurBrisable : MonoBehaviour
     IEnumerator Brise()
     {
         MurSolide.SetActive(false);
-        MurBrise.SetActive(true);
+        MurBrise.SetActive(true); 
+        foreach(GameObject childObjects in childObjects)
+        {
+            int flyup = Random.Range(5, 15);
+            childObjects.GetComponent<Rigidbody>().AddForce(new Vector3 (0,flyup,0), ForceMode.Impulse);
+            Debug.Log(flyup.ToString());
+        }
         bedingbedang.Play();
         yield return new WaitForSeconds(1.3f);
     }
