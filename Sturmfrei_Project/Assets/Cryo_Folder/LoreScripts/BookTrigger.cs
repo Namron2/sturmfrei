ad1a9a2b6c7f3103a608e8f5855fecb865ce35db
Cryosoul <oleduc08@gmail.com>
2022-04-18 14:12:02 -0400

    Lore update

---
+++ Sturmfrei_Project/Assets/Cryo_Folder/LoreScripts/BookTrigger.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookTrigger : MonoBehaviour
+{
+    public Lore lore;
+    public bool Readable = false;
+    private BookLoreManager loreManager;
+    public bool pressedDialogInput;
+    private GameObject playerObject;
+
+    //private GameObject controllerY;
+    //private GameObject keyboardF;
+
+    bool inputLore;
+    private void Start()
+    {
+        loreManager = FindObjectOfType<BookLoreManager>();
+        pressedDialogInput = false;
+        //controllerY = this.transform.Find("YController").gameObject;
+        //keyboardF = this.transform.Find("FKeyboard").gameObject;
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+
+        //controllerY.SetActive(false);
+        // keyboardF.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Readable = true;
+            loreManager.StartLore(lore);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //RemoveControleDeCheckPoint();
+            Readable = false;
+            loreManager.EndLore();
+            pressedDialogInput = false;
+        }
+    }
+
+
+    private void Update()
+    {/*
+        inputLore = Input.GetButtonDown("ReadLore");
+        if (Readable)
+        {
+            //PlayerMovement player = playerObject.GetComponent<PlayerMovement>();
+            //ControleDeCheckPoint(player);
+        }
+
+        if (Readable && inputLore && !pressedDialogInput)
+        {
+            loreManager.StartLore(lore);
+            pressedDialogInput = true;
+
+        }
+        else if (Readable && inputLore && pressedDialogInput)
+        {
+            loreManager.EndLore();
+            pressedDialogInput = false;
+        }*/
+    }
+    /*
+    private void ControleDeCheckPoint(PlayerMovement player)
+    {
+        if (controllerY != null && keyboardF != null)
+        {
+            controllerY.SetActive(true);
+            keyboardF.SetActive(true);
+
+            controllerY.transform.LookAt(player.Cam);
+            keyboardF.transform.LookAt(player.Cam);
+        }
+    }
+
+    private void RemoveControleDeCheckPoint()
+    {
+        if (controllerY != null && keyboardF != null)
+        {
+            controllerY.SetActive(false);
+            keyboardF.SetActive(false);
+        }
+    }
+    */
+}
